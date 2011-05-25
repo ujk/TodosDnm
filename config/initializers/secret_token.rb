ced3272eee6467bcc04cf29c8c688cc056256b36
ujk <ujk@ujk-Inspiron-1018.(none)>
2011-05-25 23:47:07 +0300

    ilk tasarım

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodosDnm::Application.config.secret_token = 'd53b3ca27af28c02c77ba55a81226a9505959518e69e4da567d1687baaf9cae117bd63ec1706f7115221bb700d2626a6ae64a59e64b4161fa3bc552162acb0e7'
